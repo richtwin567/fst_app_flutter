cf4aaa317fa07c20ea0eb9b489108e9020ae858f
richtwin567 <anakai.richards@gmail.com>
2020-08-03 23:12:33 -0500

    clean up and document contactDetailSvg

--- lib/widgets/contact_detail_image.dart
+++ lib/widgets/contact_detail_image.dart
@@ -1,99 +1,23 @@
-import 'dart:async' show Future;
 import 'dart:math';
-import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
 
-/* Future<String> extractAssetContent(String assetPath) async {
-  var s = await rootBundle.loadString(assetPath);
-  print(s); //for debug
-  parseSvgAssetContent(s);
-  return s;
-}
-
-parseSvgAssetContent(String svgContent) {
-  var lines = svgContent.split(new RegExp('[<,>,"]'));
-
-  lines.removeWhere((element) =>
-      element.isEmpty || element == '/' || element.contains('svg'));
-
-  List<String> parts = [];
-  lines.forEach((element) {
-    parts.add(element.trim());
-  });
-
-  int pathsStart;
-  Map<String, dynamic> properties = {};
-  for (var i = 0; i < parts.length - 2; i += 2) {
-    if (!parts[i].contains('path')) {
-      properties
-          .addAll({parts[i].substring(0, parts[i].length - 1): parts[i + 1]});
-    } else {
-      properties.addAll({
-        'path': [
-          {'d': parts[i + 1], 'commands': []}
-        ]
-      });
-      pathsStart = i + 2;
-      break;
-    }
-  }
-  int pathNum = 0;
-  for (var x = pathsStart; x < parts.length; x += 2) {
-    if (!parts[x].contains('path')) {
-      properties['path'][pathNum]
-          .addAll({parts[x].substring(0, parts[x].length - 1): parts[x + 1]});
-    } else {
-      pathNum++;
-      properties['path'].add({'d': parts[x + 1], 'commands': []});
-    }
-  }
-  for (var i = 0; i < properties['path'].length; i++) {
-    int startCommand;
-    int endCommand;
-    String commandsString = properties['path'][i]['d'];
-    int length = commandsString.length;
-    //print('full string - ' + commandsString + ' - $length');
-    for (var x = 0; x < commandsString.length; x++) {
-      if (commandsString[x].contains(new RegExp('[a-zA-Z]'))) {
-        startCommand = x;
-        //print('start - ' + commandsString[startCommand] + ' - $startCommand');
-
-        if (startCommand + 1 < commandsString.length) {
-          for (var p = startCommand + 1; p < commandsString.length; p++) {
-            if (commandsString[p].contains(new RegExp('[a-zA-Z]'))) {
-              endCommand = p;
-              //print('end - ' + commandsString[endCommand] + ' - $endCommand');
-              //print(commandsString.substring(startCommand, endCommand));
-              properties['path'][i]['commands']
-                  .add(commandsString.substring(startCommand, endCommand));
-              //print(properties['path'][i]['commands']);
-              break;
-            }
-          }
-        } else {
-          properties['path'][i]['commands']
-              .add(commandsString[commandsString.length - 1]);
-        }
-      }
-    }
-  }
-  //print(properties);
-
-  //return properties;
-}
- */
+/// Defines a point to which  a straight line should drawn in a vertical direction only.
 class VerticalPoint<T extends num> extends Point<T> {
   VerticalPoint(num y, [num x]) : super(x, y);
 }
 
+/// Defines a point to which a straight line should be drawn in a horizontal direction only.
 class HorizontalPoint<T extends num> extends Point<T> {
   HorizontalPoint(num x, [num y]) : super(x, y);
 }
 
+/// Defines a point where a path starts and the painter should therefore move to the
+/// starting point.
 class MovePoint<T extends num> extends Point<T> {
   MovePoint(T x, T y) : super(x, y);
 }
 
+/// Defines a series of points which together create a curved line.
 class CurvePoints<T extends num> extends Point<T> {
   final num x2;
   final num y2;
@@ -103,7 +27,9 @@ class CurvePoints<T extends num> extends Point<T> {
       : super(x1, y1);
 }
 
+/// The [CustomPainter] implementation of the svg version of [Icons.person].
 class ContactDetailSvg extends CustomPainter {
+  /// The background box
   List<Point<double>> path1 = [
     MovePoint(0.0, 0.0),
     HorizontalPoint(24.0, 0.0),
@@ -111,6 +37,7 @@ class ContactDetailSvg extends CustomPainter {
     HorizontalPoint(-24.0, 0.0)
   ];
 
+  /// The head
   List<Point<double>> path2 = [
     MovePoint(12.0, 12.0),
     CurvePoints(0.0, 0.0, 3.8, 0.0, 4.0, -4.0),
@@ -119,6 +46,7 @@ class ContactDetailSvg extends CustomPainter {
     CurvePoints(0.0, 0.0, 0.0, 3.8, 4.0, 4.0),
   ];
 
+  /// The bust
   List<Point<double>> path3 = [
     MovePoint(12.0, 14.0),
     CurvePoints(-2.67, 0.0, -8.0, 1.34, -8.0, 4.0),
@@ -128,14 +56,23 @@ class ContactDetailSvg extends CustomPainter {
     CurvePoints(0.0, -2.66, -5.33, -4.0, -8.0, -4.0)
   ];
 
+  /// How big the the icon should be drawn.
   final double scale;
+
+  /// the color of the icon.
   final Color color;
+
+  /// The first starting point.
   final Point start;
 
+  /// Accepts the [color] of the icon, how much to [scale] the icon by and where 
+  /// the drawing should [start]. 
   ContactDetailSvg(
       {@required this.color, this.scale = 1.0, @required this.start})
       : super();
 
+  /// Draws a path onto the [canvas] at the [start] based on the type of [points] supplied 
+  /// and the [paint] that should be used.
   drawPathFromPoints(Canvas canvas, Paint paint, List<Point> points, Point start) {
     Path path = Path();
     points.forEach((p) {
@@ -158,6 +95,7 @@ class ContactDetailSvg extends CustomPainter {
     canvas.drawPath(path, paint);
   }
 
+  /// Uses [drawPathFromPoints] to paint the icon onto the [canvas].
   @override
   void paint(Canvas canvas, Size size) {
     
