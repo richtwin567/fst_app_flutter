7d566e4af33a385c81dafd384e04ef1086c2895c
Matthew <palmer.matthew4567@gmail.com>
2020-08-23 16:03:16 -0500

    Changed ScholarshipService

--- lib/services/scholarshipservice.dart
+++ lib/services/scholarshipservice.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:async';
 import 'package:http/http.dart' as http;
-import 'package:fst_app_flutter/models/scholarship.dart';
 import 'package:fst_app_flutter/models/scholarshiplist.dart';
 
 class ScholarshipService{
@@ -9,12 +8,12 @@ class ScholarshipService{
   static String api = "http://192.168.0.7:8000/scholarship/";
   static String url = "https://www.mona.uwi.edu/osf/scholarships-bursaries";
 
-  static Future<List<Scholarship>> getAllScholarships() async {
+  static Future<ScholarshipList> getAllScholarships() async {
     try {
       var response = await http.get(api);
       if (response.statusCode == 200) {
         var jsonResponse = json.decode(response.body);
-        return Future(() => ScholarshipList.fromJson(jsonResponse).scholarList);
+        return Future(() => ScholarshipList.fromJson(jsonResponse));
       } else {
         throw Exception('Failed to Load List from Server');
       }
