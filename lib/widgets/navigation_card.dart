4c254f3d5a621be4ba5534a7cf5f6c6c8d9b3535
richtwin567 <anakai.richards@gmail.com>
2020-08-24 16:42:41 -0500

    put back in navigation function in navigation card

--- lib/widgets/navigation_card.dart
+++ lib/widgets/navigation_card.dart
@@ -20,38 +20,41 @@ class NavigationCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     MediaQueryData queryData = MediaQuery.of(context);
-    return Card(
-      elevation: 2.0,
-      shape: RoundedRectangleBorder(
-        side: BorderSide.none,
-        borderRadius: BorderRadius.circular(24),
-      ), 
-      child: Container(
-          height: height,
-          width: width,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    height: queryData.size.shortestSide * 0.1,
-                    width: queryData.size.shortestSide * 0.1,
-                    child: Center(
-                        child: Icon(
-                      icon,
-                    )),),
-                SizedBox(height: queryData.size.longestSide * 0.02),
-                Text(
-                  title,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: queryData.size.shortestSide * 0.05064),
-                  textAlign: TextAlign.center,
-                )
-              ],
-            ),
-          )),
+    return InkWell(
+      onTap: () => Navigator.pushNamed(context, route),
+          child: Card(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide.none,
+          borderRadius: BorderRadius.circular(24),
+        ), 
+        child: Container(
+            height: height,
+            width: width,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                      height: queryData.size.shortestSide * 0.1,
+                      width: queryData.size.shortestSide * 0.1,
+                      child: Center(
+                          child: Icon(
+                        icon,
+                      )),),
+                  SizedBox(height: queryData.size.longestSide * 0.02),
+                  Text(
+                    title,
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: queryData.size.shortestSide * 0.05064),
+                    textAlign: TextAlign.center,
+                  )
+                ],
+              ),
+            )),
+      ),
     );
   }
 }
